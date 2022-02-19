42c923f4688e8a432cd4fc41a1e415563555f721
James Trotter <admin@floretos.com>
2022-02-19 22:01:48 +0700

    fix: set default fan speed (#17)

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,7 @@ export default function App() {
             .get("/backend/state")
             .then((res) => {
                 setData(res.data)
-                setFanSpeed(res.data.fan || 1)
+                setFanSpeed(res.data.fan || 0)
                 setTemp(res.data.temp || 26)
                 setSwing(!!res.data.swingh || !!res.data.swingv)
                 setPower(!!res.data.power)
